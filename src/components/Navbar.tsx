2c3be0532bc3f8b696b2795a6773ffdcb189d2c1
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2024-05-07 22:45:35 +0200

    user account nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,21 +3,24 @@ import { Icons } from '@/components/Icons'
 import { Link } from '@/lib/next-intl/navigation'
 import { FC } from 'react'
 import { getAuthSession } from '@/lib/next-auth/cache'
+import UserAccountNav from '@/components/UserAccountNav'
+import { getTranslations } from 'next-intl/server'
 
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = async ({}) => {
   const session = await getAuthSession()
+  const t = await getTranslations('Project')
 
   return (
     <div className='fixed inset-x-0 top-0 h-12 border-b bg-background-100'>
       <div className='container flex h-full items-center justify-between gap-2 md:px-8'>
-        <Link href='/' className='flex items-center gap-2'>
+        <Link href='/' className='flex items-center gap-2 outline-offset-4'>
           <Icons.logo className='h-8 w-8 md:h-6 md:w-6' />
-          <p className='hidden text-sm font-medium text-foreground-700 md:block'>Breadit</p>
+          <p className='hidden text-sm font-medium text-foreground-700 md:block'>{t('title')}</p>
         </Link>
 
-        {session?.user ? <div>user nav</div> : <SignIn size='sm' />}
+        {session?.user ? <UserAccountNav user={session.user}  /> : <SignIn size='sm' />}
       </div>
     </div>
   )
---
+++ src/components/UserAccountNav.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/DropdownMenu'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/Avatar'
+import { User as UserIcon } from 'lucide-react'
+import { User } from 'next-auth'
+import { FC } from 'react'
+import { Link } from '@/lib/next-intl/navigation'
+import { signOut } from 'next-auth/react'
+import { useTranslations } from 'next-intl'
+
+interface UserAccountNavProps {
+  user: Pick<User, 'image' | 'name' | 'email'>
+}
+
+const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
+  const t = useTranslations('Components.Navbar.UserAccountNav')
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className='rounded-full'>
+        <Avatar>
+          <AvatarImage src={user.image!} alt={user.name ?? t('Trigger.alt')} />
+          <AvatarFallback>
+            <UserIcon />
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align='end'>
+        <div className='flex items-center justify-start gap-2 p-2'>
+          <div className='flex flex-col space-y-1 leading-none'>
+            {user.name && <p className='font-medium'>{user.name}</p>}
+            {user.email && <p className='w-[200px] truncate text-sm text-muted-foreground'>{user.email}</p>}
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href='/'>{t('Content.feed')}</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <Link href='/r/create'>{t('Content.create-community')}</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <Link href='/settings'>{t('Content.settings')}</Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          className='cursor-pointer'
+          onSelect={(event) => {
+            event.preventDefault()
+            signOut({
+              callbackUrl: `${window.location.origin}/sign-in`,
+            })
+          }}
+        >
+          {t('Content.sign-out')}
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+export default UserAccountNav
